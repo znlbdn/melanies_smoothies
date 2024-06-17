ed0b62b7e8f10563a53767af46edd46fab3947e8
Zainal Abidin <168837211+znlbdn@users.noreply.github.com>
2024-06-17 17:36:54 +0700

    Update streamlit_app.py
    Deactivate Session

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
+#from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
