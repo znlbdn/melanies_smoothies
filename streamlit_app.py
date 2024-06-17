b34b6ba27b8859a578e06204b44ea1c38c9caaea
Zainal Abidin <168837211+znlbdn@users.noreply.github.com>
2024-06-17 21:56:47 +0700

    Update streamlit_app.py
    Adding request API data

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,3 +51,7 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
 
         st.success('Your Smoothie is Ordered', icon="✅")
+
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
